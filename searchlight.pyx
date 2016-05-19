fba3a9feed48a91fb62ba01f0614160c4b471a79
Jeremy Muhlich <jmuhlich@bitflood.org>
2016-05-18 21:10:32 -0400

    Wrap searchlight cython function to catch zero-len inputs

--- searchlight.pyx
+++ searchlight.pyx
@@ -7,8 +7,17 @@ from libc.math cimport fabs
 DTYPE = np.float
 ctypedef np.float_t DTYPE_t
 
+def searchlight(cc1, cc2, f):
+    # The cython argument typing in searchlight_core raises an exception if cc1
+    # or cc2 are zero-length, so we need to handle that case in a non-optimized
+    # function.
+    if len(cc1) > 0 and len(cc2) > 0:
+        return searchlight_core(cc1, cc2, f)
+    else:
+        return 0
+
 @cython.boundscheck(False)
-def searchlight(DTYPE_t[:, ::1] cc1,
+def searchlight_core(DTYPE_t[:, ::1] cc1,
                 DTYPE_t[:, ::1] cc2,
                 DTYPE_t[:] f):
     assert cc1.shape[1] == cc2.shape[1] == f.shape[0]
